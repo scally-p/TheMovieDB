e030c1a7296c64cec3e44fadf5f01a148cbf0c28
Pascal Adomako <poadomako@gmail.com>
2022-12-21 09:42:49 +0100

    add homepage to details ui add fonts to textviews

--- app/src/main/java/com/scally_p/themoviedb/DetailsActivity.kt
+++ app/src/main/java/com/scally_p/themoviedb/DetailsActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.isVisible
 import com.scally_p.themoviedb.databinding.ActivityDetailsBinding
 import kotlin.math.abs
@@ -14,12 +13,13 @@ class DetailsActivity : AppCompatActivity(), OnClickListener {
     private lateinit var binding: ActivityDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.back.setOnClickListener(this)
+        binding.homepage.setOnClickListener(this)
+
         binding.appBarLayout.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
             if (abs(verticalOffset) == appBarLayout.totalScrollRange) {
                 // Collapsed
@@ -41,6 +41,9 @@ class DetailsActivity : AppCompatActivity(), OnClickListener {
         when (p0?.id) {
             R.id.back -> {
                 finish()
+            }
+            R.id.homepage -> {
+
             }
         }
     }
