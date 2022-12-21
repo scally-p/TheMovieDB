0afe9b2066be4a915d1ca502881a1b3ab749fbdf
Pascal Adomako <poadomako@gmail.com>
2022-12-21 01:44:16 +0100

    design movie details activity

---
+++ app/src/main/java/com/scally_p/themoviedb/DetailsActivity.kt
@@ -0,0 +1,47 @@
+package com.scally_p.themoviedb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.isVisible
+import com.scally_p.themoviedb.databinding.ActivityDetailsBinding
+import kotlin.math.abs
+
+class DetailsActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var binding: ActivityDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.back.setOnClickListener(this)
+        binding.appBarLayout.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            if (abs(verticalOffset) == appBarLayout.totalScrollRange) {
+                // Collapsed
+                binding.logo.isVisible = true
+                binding.poster1.isVisible = false
+            } else if (verticalOffset == 0) {
+                // Expanded
+                binding.logo.isVisible = false
+                binding.poster1.isVisible = true
+            } else {
+                // Somewhere in between
+                binding.logo.isVisible = false
+                binding.poster1.isVisible = true
+            }
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.back -> {
+                finish()
+            }
+        }
+    }
+}
--- app/src/main/java/com/scally_p/themoviedb/MainActivity.kt
+++ app/src/main/java/com/scally_p/themoviedb/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.scally_p.themoviedb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.scally_p.themoviedb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.one.content.setOnClickListener {
+            startActivity(Intent(this, DetailsActivity::class.java))
+        }
     }
 }
